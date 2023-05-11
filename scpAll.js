326b0f02ca3b4e0d16dfdbc004c96733d96392d3
Zyh <122718003+Zyhriz@users.noreply.github.com>
2023-05-11 10:39:59 -0700

    Added the possibility to run files automatically

--- scpAll.js
+++ scpAll.js
@@ -4,5 +4,13 @@ a leading newline. Check my github if you need to make one. ;)
 export async function main(ns) {
 	const file = ns.args[0];
 	const servers = ns.read("servers.txt").split("\n");
-	for(let i = 0; i < servers.length; i++){ns.scp(file,servers[i]);}
+	for(let i = 0; i < servers.length; i++){
+		ns.scp(file,servers[i]);
+		if(ns.args[1] == "run")
+		{
+			let threads = Math.floor((ns.getServerMaxRam(servers[i])-ns.getServerUsedRam(servers[i]))/ns.getScriptRam(file));
+			if(threads <= 0){continue;}
+			ns.exec(file,servers[i],threads);
+		}
+	}
 }
